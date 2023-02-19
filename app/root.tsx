39d3eb80a704be701e830e0f74879aef7fd500a2
Vitalii Motorko <vitaliimotorko@gmail.com>
2023-02-19 17:39:11 +0200

    connect fonts from google fonts

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,19 @@ export const links: LinksFunction = () => {
       rel: 'preconnect',
       href: 'https://shop.app',
     },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossOrigin: 'anonymous',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Noto+Serif:wght@700&family=Source+Sans+Pro:wght@400;600&display=swap"',
+    },
     {rel: 'icon', type: 'image/svg+xml', href: favicon},
   ];
 };
